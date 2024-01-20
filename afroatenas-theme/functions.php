8ae3a224ba69d5f32b1672917de45e1bc54b5a17
arielchmed993 <arielchmed993@gmail.com>
2024-01-20 21:48:40 +0100

    incluir boostrap cdn

--- afroatenas-theme/functions.php
+++ afroatenas-theme/functions.php
@@ -26,6 +26,31 @@ function afroatenas_setup_theme(){
 }
 add_action( 'after_setup_theme', 'afroatenas_setup_theme');
 
+
+//incluir bootstrap cdn
+function bootstrap_css() {
+	wp_enqueue_style( 'bootstrap_css', 
+  					'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css', 
+  					array(), 
+  					'4.1.3'
+  					); 
+}
+add_action( 'wp_enqueue_scripts', 'bootstrap_css');
+
+function bootstrap_js() {
+	wp_enqueue_script( 'popper_js', 
+  					'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', 
+  					array(), 
+  					'1.14.3', 
+  					true); 
+	wp_enqueue_script( 'bootstrap_js', 
+  					'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', 
+  					array('jquery','popper_js'), 
+  					'4.1.3', 
+  					true); 
+}
+add_action( 'wp_enqueue_scripts', 'bootstrap_js');
+
 function afroatenas_enqueque_scripts()
 {
     //https://afroatenas.org/wp-content/themes/afroatenas-theme/style.css
